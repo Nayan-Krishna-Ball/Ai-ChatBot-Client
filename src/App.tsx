301e5844249d8df144f76f4b755ae6ff2f7bd831
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-18 11:41:09 +0600

    first upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )} */}
+        <Route path="/chat" element={<Chat />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Chat.tsx
@@ -0,0 +1,11 @@
+//
+
+const Chat = () => {
+  return (
+    <div>
+      <h1> chat page</h1>
+    </div>
+  );
+};
+
+export default Chat;
