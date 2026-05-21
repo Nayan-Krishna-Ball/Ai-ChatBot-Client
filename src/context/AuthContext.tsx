31982f301957ee0d5f7ec9233c8561b1aadbc986
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-22 00:10:29 +0600

    Update authcontex and chat.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,7 +6,12 @@ import {
   useEffect,
   useState,
 } from "react";
-import { checkAuthStatus, loginUser } from "../helpers/api-communicator";
+import {
+  checkAuthStatus,
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../helpers/api-communicator";
 
 type User = {
   name: string;
@@ -44,18 +49,20 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setIsLoggedIn(true);
     }
   };
+
   const signup = async (name: string, email: string, password: string) => {
-    // const data = await signupUser(name, email, password);
-    // if (data) {
-    //   setUser({ email: data.email, name: data.name });
-    //   setIsLoggedIn(true);
-    // }
+    const data = await signupUser(name, email, password);
+    if (data) {
+      setUser({ email: data.email, name: data.name });
+      setIsLoggedIn(true);
+    }
   };
+
   const logout = async () => {
-    // await logoutUser();
-    // setIsLoggedIn(false);
-    // setUser(null);
-    // window.location.reload();
+    await logoutUser();
+    setIsLoggedIn(false);
+    setUser(null);
+    window.location.reload();
   };
 
   const value = {
